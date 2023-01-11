a6cacf82e0f28947d5780ad4f17e6bf4ab73ade9
kwul0208 <kwul0208@gmail.com>
2023-01-11 13:42:46 +0700

    login page

--- lib/views/pages/Auth/Login.dart
+++ lib/views/pages/Auth/Login.dart
@@ -22,8 +22,8 @@ class _LoginState extends State<Login> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
-  @override 
-  void initState(){
+  @override
+  void initState() {
     super.initState();
   }
 
@@ -37,111 +37,124 @@ class _LoginState extends State<Login> {
     width = size.width;
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SingleChildScrollView(
-          child: Column(
-        children: [
-          SizedBox(height: 50),
-          Image(
-            image: AssetImage('assets/logo-muc-tab.png'),
-            width: width / 2,
-          ),
-          SizedBox(height: 50), 
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    // enabled: false,
-                    decoration: InputDecoration(
-                      label: Text("Username"),
-                      contentPadding: const EdgeInsets.symmetric(
-                          vertical: .0, horizontal: 10.0),
-                      fillColor: Colors.transparent,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        borderSide: BorderSide(
-                          color: Colors.grey[400]!,
-                          width: 1.0,
-                        ),
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Username is requied!';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    obscureText: _isObscure,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      suffixIcon: IconButton(
-                        icon: Icon(_isObscure ? Icons.visibility : Icons.visibility_off),
-                        onPressed: () {
-                          setState(() {
-                            _isObscure = !_isObscure;
-                          });
-                        }
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                          vertical: .0, horizontal: 10.0),
-                      fillColor: Colors.transparent,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        borderSide: BorderSide(
-                          color: Colors.grey[400]!,
-                          width: 1.0,
-                        ),
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Password is required!';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 40),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Config().primary,
-                      minimumSize: const Size.fromHeight(50), // NEW
-                    ),
-                    onPressed: _loadBtn == false ? () async {
-                      if (_formKey.currentState!.validate()) {
-                          setState(() {
-                            _loadBtn = !_loadBtn;
-                          });
-                          await Future.delayed(const Duration(seconds: 2));
-                          print('object');
-                          setState(() {
-                            _loadBtn = !_loadBtn;
-                          });
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => Timesheet()));
-                      }
-                    } : null,
-                    child: _loadBtn == false ?  Text(
-                      'Login',
-                      style: TextStyle(fontSize: 24),
-                    ) :  CircularProgressIndicator(),
-                  ),
-                ],
-              ),
+          child: Container(
+        width: width,
+        height: height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image(
+              image: AssetImage('assets/logo-muc-tab.png'),
+              width: width / 2,
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      // enabled: false,
+                      decoration: InputDecoration(
+                        label: Text("Username"),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: .0, horizontal: 10.0),
+                        fillColor: Colors.transparent,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(
+                            color: Colors.grey[400]!,
+                            width: 1.0,
+                          ),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Username is requied!';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      obscureText: _isObscure,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        suffixIcon: IconButton(
+                            icon: Icon(_isObscure
+                                ? Icons.visibility
+                                : Icons.visibility_off),
+                            onPressed: () {
+                              setState(() {
+                                _isObscure = !_isObscure;
+                              });
+                            }),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: .0, horizontal: 10.0),
+                        fillColor: Colors.transparent,
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(
+                            color: Colors.grey[400]!,
+                            width: 1.0,
+                          ),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Password is required!';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 40),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Config().primary,
+                        minimumSize: const Size.fromHeight(50), // NEW
+                      ),
+                      onPressed: _loadBtn == false
+                          ? () async {
+                              if (_formKey.currentState!.validate()) {
+                                setState(() {
+                                  _loadBtn = !_loadBtn;
+                                });
+                                await Future.delayed(
+                                    const Duration(seconds: 2));
+                                print('object');
+                                setState(() {
+                                  _loadBtn = !_loadBtn;
+                                });
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Timesheet()));
+                              }
+                            }
+                          : null,
+                      child: _loadBtn == false
+                          ? Text(
+                              'Login',
+                              style: TextStyle(fontSize: 24),
+                            )
+                          : CircularProgressIndicator(),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       )),
     );
   }
