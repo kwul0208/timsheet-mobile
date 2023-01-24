0272fc204bc1ba87fad4ca3b56a7e5a847ca3632
kwul0208 <kwul0208@gmail.com>
2023-01-24 15:27:24 +0700

    implement delete api

--- lib/views/pages/Timsheet/DetailTimesheet.dart
+++ lib/views/pages/Timsheet/DetailTimesheet.dart
@@ -1,16 +1,13 @@
+import 'dart:async';
+import 'dart:convert';
+
 import "package:flutter/material.dart";
 import 'package:timsheet_mobile/Config/Config.dart';
 import 'package:timsheet_mobile/Helper/Helper.dart';
 import 'package:timsheet_mobile/views/pages/Timsheet/EditTimesheet.dart';
+import 'package:http/http.dart' as http;
 
-// Date Input	:	23/01/2023 02:45:07
-// Date	:	20/01/2023
-// Time	:	08:00-09:00
-// Duration	:	01:00 - Daily Routine
-// Description	:	fix bug color (timesheet app)
-// Revision Date	:	-
-
-class DetailTimesheet extends StatelessWidget {
+class DetailTimesheet extends StatefulWidget {
   const DetailTimesheet({super.key, required this.id, required this.date, required this.date_input,required this.timeStart, required this.timeEnd, required this.time_duration, required this.desc, required this.date_modified, required this.tmode_name, required this.tmode_id});
 
   final int id;
@@ -24,11 +21,110 @@ class DetailTimesheet extends StatelessWidget {
   final String tmode_name;
   final int tmode_id;
 
-  
+  @override
+  State<DetailTimesheet> createState() => _DetailTimesheetState();
+}
+
+class _DetailTimesheetState extends State<DetailTimesheet> {
+    // status loading
+  bool _isLoading = false;
+  String _isStatus = 'false';
+  // endStatus loading
+
+
+  _showConfirm (){
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user must tap button!
+      builder: (BuildContext context) {
+          return StatefulBuilder(
+            builder: (context, setState) {
+              return AlertDialog(
+                title: Text("Hapus"),
+                content: SingleChildScrollView(
+                  child: ListBody(
+                    children: <Widget>[
+                      _isStatus == 'false' ? Text("Are You Sure?") :
+                        _isStatus == 'load' ?
+                        Center(
+                          child: CircularProgressIndicator()) :
+                          _isStatus == 'success' ?
+                          Center(
+                          child: Column(
+                            children: [
+                              Icon(Icons.check_circle, size: 50, color: Color.fromRGBO(47, 158, 95, 1),),
+                              Text('Berhasil')
+                            ],
+                          )) : SizedBox()
+                    ],
+                  ),
+                ),
+                actions: <Widget>[
+                _isStatus == 'false' ?
+                  TextButton(
+                    child: const Text('ok'),
+                    onPressed: () {
+                      setState(() {
+                        _isStatus = 'load';
+                      });
+                      deleteTimesheet(widget.id).then((value){
+                        if (value['status'] == true) {
+                          setState(() {
+                            _isStatus = 'success';
+                          });
+                          Timer(Duration(seconds: 1), (){
+                            Navigator.pop(context);
+                            Navigator.pop(context, widget.date);
+                          });
+                        } else {
+                          setState(() {
+                            _isLoading = false;
+                          });
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Gagal! Silahkan coba beberapa saat lagi"),
+                          ));
+                        }
+                      });
+                      // deleteProduct(widget.id.toString()).then((value) {
+                      //   if (value['status'] == 200) {
+                          // setState(() {
+                          //   _isStatus = 'success';
+                          // });
+                          // Timer(Duration(seconds: 1), (){
+                          //   Navigator.pop(context);
+                          //   Navigator.pop(context, 'back');
+                          // });
+                          // Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp(page: 3,)),);
+                      //   } else {
+                          // setState(() {
+                          //   _isLoading = false;
+                          // });
+                          // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          //   content: Text("Gagal! Silahkan coba beberapa saat lagi"),
+                          // ));
+                      //   }
+                      // });
+                    },
+                  ) : SizedBox(),
+                _isStatus == 'false' ?
+                 TextButton(
+                  child: const Text('batal'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ) : SizedBox(),
+              ],
+              );
+
+            }
+          );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-  print(tmode_id);
+  print(widget.tmode_id);
 
     return Scaffold(
       backgroundColor: Colors.white,
@@ -38,13 +134,18 @@ class DetailTimesheet extends StatelessWidget {
         title: const Text("Datail Timesheet",
             style: TextStyle(color: Colors.black, fontSize: 18)),
         centerTitle: true,
-        actions: const [
-          Padding(
-            padding: const EdgeInsets.only(right: 10),
-            child: Icon(
-              Icons.delete,
-              color: Colors.red,
-              size: 30,
+        actions:  [
+          GestureDetector(
+            onTap: (){
+              _showConfirm();
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: Icon(
+                Icons.delete,
+                color: Colors.red,
+                size: 30,
+              ),
             ),
           )
         ],
@@ -63,11 +164,11 @@ class DetailTimesheet extends StatelessWidget {
                   shrinkWrap: true,
                   physics: NeverScrollableScrollPhysics(),
                   children: [
-                    CardDetailTimesheet(title: "Date Input : ", value: "$date_input",),
-                    CardDetailTimesheet(title: "Date : ", value: "$date",),
-                    CardDetailTimesheet(title: "Duration :", value: "${Helper().formatedTime(time: time_duration)} - Daily Routine",),
-                    CardDetailTimesheet(title: "Description	:", value: "$desc",),
-                    CardDetailTimesheet(title: "Revision Date	:", value: "$date_modified",),
+                    CardDetailTimesheet(title: "Date Input : ", value: "${widget.date_input}",),
+                    CardDetailTimesheet(title: "Date : ", value: "${widget.date}",),
+                    CardDetailTimesheet(title: "Duration :", value: "${Helper().formatedTime(time: widget.time_duration)} - Daily Routine",),
+                    CardDetailTimesheet(title: "Description	:", value: "${widget.desc}",),
+                    CardDetailTimesheet(title: "Revision Date	:", value: "${widget.date_modified}",),
                   ]
                 ),
                 ElevatedButton(
@@ -76,7 +177,7 @@ class DetailTimesheet extends StatelessWidget {
                         minimumSize: const Size.fromHeight(50), // NEW
                       ),
                       onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => EditTimesheet(id: id, date: date, desc: desc, timeStart: timeStart, timeEnd: timeEnd, tmode_id: tmode_id,)));
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => EditTimesheet(id: widget.id, date: widget.date, desc: widget.desc, timeStart: widget.timeStart, timeEnd: widget.timeEnd, tmode_id: widget.tmode_id,)));
                       },
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.center,
@@ -97,6 +198,34 @@ class DetailTimesheet extends StatelessWidget {
       )
     );
   }
+
+  // ---------- API -----------
+  String baseUrl = Config().url;
+  Future deleteTimesheet(int timesheetId)async{
+    var headers = {
+      'Content-Type': 'application/json'
+    };
+    var request = http.Request('POST', Uri.parse('$baseUrl/mucnet_api/api/timesheet/delete'));
+    request.body = json.encode({
+      "timesheet_id": timesheetId
+    });
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print('y');
+      print(await response.stream.bytesToString());
+      return {"status": true, "message": "success"};
+    }
+    else {
+      print('x');
+      print(await response.stream.bytesToString());
+      print(response.reasonPhrase);
+      return {"status": true, "message": "${response.reasonPhrase}"};
+    }
+  }
+
 }
 
 class CardDetailTimesheet extends StatelessWidget {
