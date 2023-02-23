3798e507cf540b9f3145e4a2e98885d2af583a3d
kwul0208 <kwul0208@gmail.com>
2023-02-23 13:58:41 +0700

    leave & icon apps

--- lib/views/menu/Cuti.dart
+++ lib/views/menu/Cuti.dart
@@ -2,6 +2,7 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 import 'package:flutter/material.dart';
+import 'package:timsheet_mobile/Config/Config.dart';
 
 class Cuti extends StatefulWidget {
   const Cuti({super.key});
@@ -14,7 +15,22 @@ class _CutiState extends State<Cuti> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Cuti")),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Config().primary,
+        title: Text("Leave", style: TextStyle(fontSize: 21, fontWeight: FontWeight.w800),),
+        centerTitle: true,  
+      ),
+      body: Center(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/maintenance.png"),
+            Text("This page is under construction", style: TextStyle(fontSize: 21, fontWeight: FontWeight.w400),)
+          ],
+        ),
+      ),
     );
   }
 }
